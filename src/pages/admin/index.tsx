936d69c89851ef6b2446d8810ae8c66ebe977c35
iron maiden <ruancarlosdejesus8@gmail.com>
2025-03-21 19:50:38 -0300

    criando um arquivo vercel

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -81,7 +81,11 @@ if( nameInput ==="" || url===""){
     return (    
             <div className="flex items-center flex-col min--screen pb-7 px-2">
                 <Header/>
+
+
                 <form className="flex flex-col mt-8 mb-3 w-full max-w-xl" onSubmit={handleRegister}>
+
+                    
 
                     <label className="text-white font-medium mt-2 mb-2"> 
                        Nome do Link
